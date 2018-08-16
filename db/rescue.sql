f79d82c56843bc5ff41a7f412cb9a4c1c264fe46
Mohamed Riyas <mohamed.riyas@orangekloud.com>
2018-08-16 21:39:40 +0530

    Note field added and Name made mandatory

--- db/rescue.sql
+++ db/rescue.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 16, 2018 at 02:00 PM
+-- Generation Time: Aug 16, 2018 at 04:09 PM
 -- Server version: 5.5.54
 -- PHP Version: 5.4.45
 
@@ -34,9 +34,10 @@ CREATE TABLE IF NOT EXISTS `markers` (
   `lat` float(16,6) NOT NULL,
   `lng` float(10,6) NOT NULL,
   `type` varchar(30) DEFAULT 'Stranded',
+  `note` text NOT NULL,
   `added_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
